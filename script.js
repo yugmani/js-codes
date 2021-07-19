aaada2dee232799d067f559ddb474778e8baed31
yugmani <yugmani@yahoo.com>
2021-07-18 23:56:08 -0700

    concatenating two or more arrays without causing server overload

--- script.js
+++ script.js
@@ -29,3 +29,20 @@ let last_name = 'Bhattarai';
 
 // console.log('First Name: ', first_name); // First Name:  Bhattarai
 // console.log('Last_name: ', last_name); // Last_name:  Baburam
+
+// Concatenating two or more arrays
+
+let a = [1,2,3];
+let b = [4,5,6];
+
+// causing server overload
+let c = a.concat(b);
+console.log(c);
+// (6) [1, 2, 3, 4, 5, 6]
+
+// Without causeing server overload
+a.push.apply(a,b);
+console.log("a: ", a);
+// a:  (6) [1, 2, 3, 4, 5, 6]
+
+
