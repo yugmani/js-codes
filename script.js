0d5a7d3d70bc8048fac759f2659120c85629ee7a
yugmani <yugmani@yahoo.com>
2021-07-19 00:04:31 -0700

    filter(Boolean).filter() method

--- script.js
+++ script.js
@@ -32,17 +32,34 @@ let last_name = 'Bhattarai';
 
 // Concatenating two or more arrays
 
-let a = [1,2,3];
-let b = [4,5,6];
+let a = [1, 2, 3];
+let b = [4, 5, 6];
 
 // causing server overload
 let c = a.concat(b);
-console.log(c);
+// console.log(c);
 // (6) [1, 2, 3, 4, 5, 6]
 
 // Without causeing server overload
-a.push.apply(a,b);
-console.log("a: ", a);
+a.push.apply(a, b);
+// console.log("a: ", a);
 // a:  (6) [1, 2, 3, 4, 5, 6]
 
+// New way of using filter()
 
+const array1 = [
+  null,
+  undefined,
+  { name: 'Keshar' },
+  { name: 'Mamata' },
+  { name: '' },
+  null
+];
+
+// const newArray1 = array1.filter(item => item.name.length = 0);
+//this will show error Uncaught TypeError: Cannot read property 'length' of undefined
+
+const newArray1 = array1.filter(Boolean).filter(item => item.name.length > 0);
+
+console.log(newArray1);
+// [{ name: 'Keshar' }, { name: 'Mamata' }]
