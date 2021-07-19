e772a60a86cda5689537ba2dfa4e28a0cd0fde46
yugmani <yugmani@yahoo.com>
2021-07-18 23:47:20 -0700

    resize array

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log('hello!')
+// *********************************
+// Javascript Hacks
+// *********************************
+
+// ***** Resize the array ******
+const cities = [
+  'New York',
+  'Dallas',
+  'Washington DC',
+  'Boston',
+  'Los Angeles',
+  'Las Vegas'
+];
+
+// console.log(cities.length); // 6
+
+cities.length = 3;
+// console.log(cities);
+// (3) ["New York", "Dallas", "Washington DC"]
