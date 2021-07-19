a0e927e545f955f4fe58f559b377343d324d1799
yugmani <yugmani@yahoo.com>
2021-07-18 23:51:32 -0700

    swapping of two values

--- script.js
+++ script.js
@@ -17,3 +17,15 @@ const cities = [
 cities.length = 3;
 // console.log(cities);
 // (3) ["New York", "Dallas", "Washington DC"]
+
+// Swapping of two values;
+
+let first_name = 'Baburam';
+let last_name = 'Bhattarai';
+// console.log('First Name: ', first_name); //  First Name:  Baburam
+// console.log('Last_name: ', last_name); // Last_name:  Bhattarai
+
+[first_name, last_name] = [last_name, first_name];
+
+// console.log('First Name: ', first_name); // First Name:  Bhattarai
+// console.log('Last_name: ', last_name); // Last_name:  Baburam
